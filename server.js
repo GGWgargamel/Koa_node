70858f8905d252ec26c8a601b19e719821eaab93
guangenwei <guangw@fuwu.work>
2019-05-08 10:30:53 +0800

    test

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ mongoose.connect('mongodb://localhost:27017/jr', {
 
 const Koa = require('koa')
 const app = new Koa()
+const path = require('path');
+const serve = require('koa-static');
+const main = serve(path.join(__dirname+'/h5'));
+app.use(main);
 // babyparser用于处理post
 const bobyParser  = require('koa-bodyparser')
 app.use(bobyParser())
@@ -19,6 +23,6 @@ app.use(bobyParser())
 const router = require('./config/router')()
 app.use(router.routes())
 app.use(router.allowedMethods())
-app.listen(3000)
-
-console.log('app started at port 3000')
+app.listen(3000, function() {
+  console.log('服务启动')
+})
